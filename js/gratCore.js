b81033d2ea03d707f9647c6a22f9c3f7db5e7a8c
dantoni.massimo <dantoni.massimo@c400dantoni.aoud.sanita.fvg.it>
2015-07-29 17:21:49 +0200

    implementazione grafico via ajax completo

--- js/gratCore.js
+++ js/gratCore.js
@@ -5,20 +5,20 @@
  * e se lo trova imposta il messaggio sull'elemento
  */
 
-Drupal.behaviors.gratCore = function(context) {
+Drupal.behaviors.gratCore = function (context) {
 // qtip connection
-    $jq('[tipurl]').each(function (){
+    $jq('[tipurl]').each(function () {
         $jq(this).qtip({
-            content: {  
+            content: {
                 title: 'info rapide',
                 text: 'caricamento...',
-            ajax : {url: '?q=' + $jq(this).attr('tipurl')}
+                ajax: {url: '?q=' + $jq(this).attr('tipurl')}
             },
-            show: {event: 'mouseenter' ,
+            show: {event: 'mouseenter',
                 solo: true
             },
             hide: {
-                event:'mouseleave',
+                event: 'mouseleave',
                 fixed: true
             },
             position: {
@@ -33,171 +33,209 @@ Drupal.behaviors.gratCore = function(context) {
         })
     });
 // treeview connection
-    $jq('#tree-message').each(function (){
+    $jq('#tree-message').each(function () {
         $jq(this).qtip(
-        {
-            id: 'modal', // Since we're only creating one modal, give it an ID so we can style it
-            content: {
-                text: $jq('div.treeview'),
-                title: {
-                    text: $jq(this).attr('dialogModal'),
-                    button: true
-                }
-            },
-            position: {
-                my: 'top center', // ...at the center of the viewport
-                at: 'top center',
-                target: $jq(window)
-            },
-            show: {
-                event: 'click', // Show it on click...
-                solo: true, // ...and hide all other tooltips...
-                modal: true // ...and make it modal
-            },
-            hide: false,
-            style: { 
-                classes: 'ui-tooltip-light ui-tooltip-rounded',
-                width: '760px'
-            },
-            events: {
-                hide: function(event, api){
-                    var list ='';
-                    var struct_selection='';
-                    $jq('ul.treeview input[type="checkbox"]:checked').each(function (){
-                        list=list + (list==''? '' : ', ') + $jq(this).parent().text();
-                        struct_selection = struct_selection + (struct_selection==''? '' : ', ') + $jq(this).val();
-                    });
-                    $jq('input[name="struct_selection"]').val(struct_selection);
-                    if(list=='') list='Selezione struttura';
-                    $jq('a#tree-message').text(list);
-                
-                }
-            }
-        });
+                {
+                    id: 'modal', // Since we're only creating one modal, give it an ID so we can style it
+                    content: {
+                        text: $jq('div.treeview'),
+                        title: {
+                            text: $jq(this).attr('dialogModal'),
+                            button: true
+                        }
+                    },
+                    position: {
+                        my: 'top center', // ...at the center of the viewport
+                        at: 'top center',
+                        target: $jq(window)
+                    },
+                    show: {
+                        event: 'click', // Show it on click...
+                        solo: true, // ...and hide all other tooltips...
+                        modal: true // ...and make it modal
+                    },
+                    hide: false,
+                    style: {
+                        classes: 'ui-tooltip-light ui-tooltip-rounded',
+                        width: '760px'
+                    },
+                    events: {
+                        hide: function (event, api) {
+                            var list = '';
+                            var struct_selection = '';
+                            $jq('ul.treeview input[type="checkbox"]:checked').each(function () {
+                                list = list + (list == '' ? '' : ', ') + $jq(this).parent().text();
+                                struct_selection = struct_selection + (struct_selection == '' ? '' : ', ') + $jq(this).val();
+                            });
+                            $jq('input[name="struct_selection"]').val(struct_selection);
+                            if (list == '')
+                                list = 'Selezione struttura';
+                            $jq('a#tree-message').text(list);
+
+                        }
+                    }
+                });
     });
 
-    $jq('ul.treeview').each(function (){
+    $jq('ul.treeview').each(function () {
         $jq(this).treeview({
             /* collapsed:false */
-            }) 
+        })
     });
     /*
- * seleziona o deseleziona tutti i checkbox figli 
- * */
-    $jq('ul.treeview input[type="checkbox"]').each(function(){
-    
-        $jq(this).click(function (){
+     * seleziona o deseleziona tutti i checkbox figli 
+     * */
+    $jq('ul.treeview input[type="checkbox"]').each(function () {
+
+        $jq(this).click(function () {
             var parent_id = $jq(this).attr('value'); /* il valore del checkbox è anche l'id del contenitore del sottoalbero*/
-            var check=$jq(this).attr('checked');
+            var check = $jq(this).attr('checked');
             /* reimposta tutti i checkbox nell'<UL> contenuto  in questo li identificato con id*/
-            $jq('ul.treeview li#'+parent_id + ' ul input[type="checkbox"]').attr('checked',check);
+            $jq('ul.treeview li#' + parent_id + ' ul input[type="checkbox"]').attr('checked', check);
         });
-    
+
     });
-   // limitatore caratteri  
-   $('input[type="text"]').bind('keypress',function(event){
-       var regint = new RegExp("[0-9]");//caratteri concessi per gli interi
-       var regcmd= new RegExp("[\x08-\x2E]"); //filtra i comandi della tastiera
-       var $this=$(this).val().length;
-       var maxlenght=$(this).attr("size");//recupera dimensione
-       var key = String.fromCharCode(!event.charCode ? event.which : event.charCode); //tasto catturato 
-       
-       if (!regint.test(key) & !regcmd.test(key) & $(this).hasClass('integer')) {
+    // limitatore caratteri  
+    $('input[type="text"]').bind('keypress', function (event) {
+        var regint = new RegExp("[0-9]");//caratteri concessi per gli interi
+        var regcmd = new RegExp("[\x08-\x2E]"); //filtra i comandi della tastiera
+        var $this = $(this).val().length;
+        var maxlenght = $(this).attr("size");//recupera dimensione
+        var key = String.fromCharCode(!event.charCode ? event.which : event.charCode); //tasto catturato 
+
+        if (!regint.test(key) & !regcmd.test(key) & $(this).hasClass('integer')) {
             event.preventDefault();
             return false;
-       }
+        }
 //       if ($this >= maxlenght & !regcmd.test(key)){
 //           event.preventDefault();
 //           return false;
 //       }
     });
-    
-  // sparkline connection
-    
-  $jq('.sparkline').each(function(){
-      // var settings=Drupal.gratcore.sparklineSettings;
-      this.sparkline('html', { enableTagOptions: true });
-  });
-    
-    
-    // jqplot connections and get data
-    //retreive settings for plots, it is an array with:
-    // [ html-id -- [ 'data' -- [array of data]
-    //                'settings' --[json object with dowing options]
-    //              ]
-    // ]
-   
+
+    // sparkline connection
+
+    $jq('.sparkline').each(function () {
+        // var settings=Drupal.gratcore.sparklineSettings;
+        this.sparkline('html', {enableTagOptions: true});
+    });
 
 
-      var plots=Drupal.settings.plots;
-      $jq.jqplot.config.enablePlugins = true;
-      for (var i in plots){
-        var options=plots[i]['settings'];
-        // valuta i plugin
-        try {options.seriesDefaults.renderer=eval(options.seriesDefaults.renderer);} catch (error){}
-        try {options.axesDefaults.labelRenderer=eval(options.axesDefaults.labelRenderer);} catch (error){}
-        try {options.axes.xaxis.renderer=eval(options.axes.xaxis.renderer);} catch (error){}
-        try {options.axes.yaxis.renderer=eval(options.axes.yaxis.renderer);} catch (error){}
-        try {options.legend.renderer=eval(options.legend.renderer);} catch (error){}
-             //mancano alcuni plugin...
-        
-        
-        var obj= plots[i]['data'];
-        var  dt =[]
-        for( var j in obj){
-            dt[j]=obj[j]
-        }
-        
-        $jq.jqplot(i, dt ,options);
-      }
-        //need to bind showand hide container events 
+
+
+
+
+    //need to bind showand hide container events 
 //      $('fieldset.collapsible').bind('show',function (event,ui){
 //          plot.replot();
 //      });
 
- // dynamic ajax plot
 
-//$jq('div.plot[src]').each( function () {
-//    var url = '?q=' + $jq(this).attr('src');
-//    var ajaxData = function(){
-//        var ret = nul;
-//        $jq.ajax({
-//            async: false,
-//            url: url,
-//            dataType: "json",
-//            success: function(data){
-//                ret=data;
-//            }
-//        });
-//
-//        return ret;
-//    };
-//    var plot = ajaxData();
-//    $jq.jqplot.config.enablePlugins = true;
-//        var options=plots['settings'];
-//        // valuta i plugin
-//        try {options.seriesDefaults.renderer=eval(options.seriesDefaults.renderer);} catch (error){}
-//        try {options.axesDefaults.labelRenderer=eval(options.axesDefaults.labelRenderer);} catch (error){}
-//        try {options.axes.xaxis.renderer=eval(options.axes.xaxis.renderer);} catch (error){}
-//        try {options.axes.yaxis.renderer=eval(options.axes.yaxis.renderer);} catch (error){}
-//        try {options.legend.renderer=eval(options.legend.renderer);} catch (error){}
-//             //mancano alcuni plugin...
-//        
-//        
-//        var obj= plots['data'];
-//        var  dt =[]
-//        for( var j in obj){
-//            dt[j]=obj[j]
-//        }
-//        //check id contenitore
-//        var id = $jq(this).attr('id');
-//        if(typeof id === typeof undefined || id === false) {
-//            id=randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-//            $jq(this).setAttribute('id',id)
-//        }
-//        $jq.jqplot(id, dt ,options);
-//      }
-//});
+    // dynamic ajax plot 
+    // data array contain 2 keys : data and options
+    // options is jqplot options
 
+    $jq('div.plot[src]').each(function () {
+        var url = '?q=' + $jq(this).attr('src');
+        var ajaxData = function () {
+            var ret = null;
+            $jq.ajax({
+                async: false,
+                url: url,
+                dataType: "json",
+                success: function (data) {
+                    ret = data;
+                },
+                error: function (err, status, msg) {
+                    alert(msg + ' (' + status + ')');
+                }
+            });
+
+            return ret;
+        };
+        var plot = ajaxData();
+        if (plot) {
+            $jq.jqplot.config.enablePlugins = true;
+            var options = plot['options'];
+            // valuta i plugin
+            try {
+                options.seriesDefaults.renderer = eval(options.seriesDefaults.renderer);
+            } catch (error) {
+            }
+            try {
+                options.axesDefaults.labelRenderer = eval(options.axesDefaults.labelRenderer);
+            } catch (error) {
+            }
+            try {
+                options.axes.xaxis.renderer = eval(options.axes.xaxis.renderer);
+            } catch (error) {
+            }
+            try {
+                options.axes.yaxis.renderer = eval(options.axes.yaxis.renderer);
+            } catch (error) {
+            }
+            try {
+                options.legend.renderer = eval(options.legend.renderer);
+            } catch (error) {
+            }
+            //mancano alcuni plugin...
+
+
+            var obj = plot['data'];
+            var dt = []
+            for (var j in obj) {
+                dt[j] = obj[j]
+            }
+            //check id contenitore
+            var id = $jq(this).attr('id');
+            if (typeof id === typeof undefined || id === false) {
+                //set random id 
+                id = new Array(5).join().replace(/(.|$)/g, function () {
+                    return ((Math.random() * 36) | 0).toString(36);
+                })
+                $jq(this).attr('id', id)
+            }
+            $jq.jqplot(id, dt, options);
+        }
+    });
+
+//  BEGIN  --- impleentazione statica da eliminare progressivamente  
+    var plots = Drupal.settings.plots;
+    $jq.jqplot.config.enablePlugins = true;
+    for (var i in plots) {
+        var options = plots[i]['settings'];
+        // valuta i plugin
+        try {
+            options.seriesDefaults.renderer = eval(options.seriesDefaults.renderer);
+        } catch (error) {
+        }
+        try {
+            options.axesDefaults.labelRenderer = eval(options.axesDefaults.labelRenderer);
+        } catch (error) {
+        }
+        try {
+            options.axes.xaxis.renderer = eval(options.axes.xaxis.renderer);
+        } catch (error) {
+        }
+        try {
+            options.axes.yaxis.renderer = eval(options.axes.yaxis.renderer);
+        } catch (error) {
+        }
+        try {
+            options.legend.renderer = eval(options.legend.renderer);
+        } catch (error) {
+        }
+        //mancano alcuni plugin...
+
+
+        var obj = plots[i]['data'];
+        var dt = []
+        for (var j in obj) {
+            dt[j] = obj[j]
+        }
+
+        $jq.jqplot(i, dt, options);
+    }
+    //  END  --- impleentazione statica da eliminare progressivamente  
 };// end Drupal.behaviours...
 
